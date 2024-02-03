14c5544219bb19963a6a0957472a01466bddeba5
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 15:15:39 +0100

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.enable_tab_bar = false
+config.color_scheme = "Catppuccin Mocha"
+config.font_size = 13.8
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.window_background_opacity = 0.95
+
+return config
